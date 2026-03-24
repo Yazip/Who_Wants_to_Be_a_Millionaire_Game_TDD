ebd7628287f13df0a3b121bf0ebdd09410f12699
Yazip <USAASU386@mail.ru>
2026-03-24 18:03:25 +0700

    Refactored set_correct_answer_index() and get_correct_answer_index() methods, added correct_answer_index field. Another set correct answer index test successes

--- src/question.cpp
+++ src/question.cpp
@@ -1,10 +1,9 @@
 #include "question.h"
 
 void Question::set_correct_answer_index(int correct_answer_index) {
-	// TODO: реализовать полноценное установление индекса правильного ответа
+	this->correct_answer_index = correct_answer_index;
 }
 
 int Question::get_correct_answer_index() const {
-	// TODO: реализовать полноценный возврат индекса правильного ответа
-	return 0;
+	return this->correct_answer_index;
 }
--- src/question.h
+++ src/question.h
@@ -1,6 +1,8 @@
 #pragma once
 
 class Question {
+private:
+	int correct_answer_index;
 public:
 	Question() = default;
 
