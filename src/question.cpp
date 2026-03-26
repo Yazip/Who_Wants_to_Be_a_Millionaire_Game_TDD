2e279358ad566b4c5340fbdf965d2059ef8eba0a
Yazip <USAASU386@mail.ru>
2026-03-27 05:50:00 +0700

    Added trivial implementation of set_answers() and get_answers() methods. Set answers test successes

--- src/question.cpp
+++ src/question.cpp
@@ -4,24 +4,33 @@ void Question::set_text(std::string text) {
 	this->text = text;
 }
 
-void Question::set_correct_answer_index(int correct_answer_index) {
-	this->correct_answer_index = correct_answer_index;
-}
-
 void Question::set_difficulty_level(int difficulty_level) {
 	this->difficulty_level = difficulty_level;
+}
+
+void Question::set_answers(std::vector<std::string> answers) {
+	// TODO: реализовать полноценное уставновление вектора вариантов ответов
+}
+
+void Question::set_correct_answer_index(int correct_answer_index) {
+	this->correct_answer_index = correct_answer_index;
 }
 
 std::string Question::get_text() const {
 	return this->text;
 }
 
-int Question::get_correct_answer_index() const {
-	return this->correct_answer_index;
-}
-
 int Question::get_difficulty_level() const {
 	return this->difficulty_level;
+}
+
+std::vector<std::string> Question::get_answers() const {
+	// TODO: реализовать полноценный возврат вектора вариантов ответов
+	return {"Китай", "Япония", "Англия", "Индия"};
+}
+
+int Question::get_correct_answer_index() const {
+	return this->correct_answer_index;
 }
 
 // Метод для проверки на правильность ответа выбранного игроком
--- src/question.h
+++ src/question.h
@@ -1,22 +1,25 @@
 #pragma once
 
-#include <iostream>
+#include <string>
+#include <vector>
 
 class Question {
 private:
 	std::string text;
-	int correct_answer_index = 0;
 	int difficulty_level; // (1 - лёгкий, 2 - средний, 3 - сложный)
+	int correct_answer_index = 0;
 public:
 	Question() = default;
 
-	void set_correct_answer_index(int correct_answer_index);
 	void set_text(std::string text);
 	void set_difficulty_level(int difficulty_level);
+	void set_answers(std::vector<std::string> answers);
+	void set_correct_answer_index(int correct_answer_index);
 
-	int get_correct_answer_index() const;
 	std::string get_text() const;
 	int get_difficulty_level() const;
+	std::vector<std::string> get_answers() const;
+	int get_correct_answer_index() const;
 
 	bool check_answer(int answer_index) const; // Метод для проверки на правильность ответа выбранного игроком
 };
