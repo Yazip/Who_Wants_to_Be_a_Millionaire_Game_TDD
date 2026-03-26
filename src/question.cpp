362718a92fdb853c536280eeb921d3df84e84ee1
Yazip <USAASU386@mail.ru>
2026-03-27 04:35:28 +0700

    Added trivial implementation of set_difficulty_level() and get_difficulty_level() methods. Set difficulty level test successes

--- src/question.cpp
+++ src/question.cpp
@@ -8,12 +8,21 @@ void Question::set_correct_answer_index(int correct_answer_index) {
 	this->correct_answer_index = correct_answer_index;
 }
 
+void Question::set_difficulty_level(int difficulty_level) {
+	// TODO: реализовать полноценное установление уровня сложности вопроса
+}
+
 std::string Question::get_text() const {
 	return this->text;
 }
 
 int Question::get_correct_answer_index() const {
 	return this->correct_answer_index;
+}
+
+int Question::get_difficulty_level() const {
+	// TODO: реализовать полноценный возврат уровня сложности вопроса
+	return 1;
 }
 
 // Метод для проверки на правильность ответа выбранного игроком
--- src/question.h
+++ src/question.h
@@ -11,9 +11,11 @@ public:
 
 	void set_correct_answer_index(int correct_answer_index);
 	void set_text(std::string text);
+	void set_difficulty_level(int difficulty_level);
 
 	int get_correct_answer_index() const;
 	std::string get_text() const;
+	int get_difficulty_level() const;
 
 	bool check_answer(int answer_index) const; // Метод для проверки на правильность ответа выбранного игроком
 };
